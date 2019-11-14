84d4ecc276d350fd4778f05a926f2e6a85c92b20
Mega <Aza.Noriega@gmail.com>
2019-11-14 05:17:11 +0500

    finish uploadAddressProgram()

--- src/api/uploadAddressProgram.js
+++ src/api/uploadAddressProgram.js
@@ -2,26 +2,37 @@ const XLSX = require('xlsx')
 
 module.exports = uploadAddressProgram
 
-async function uploadAddressProgram({ name, file }, callback) {
-  try {
-    const db = await require('../getMongoDB')()
+async function uploadAddressProgram({ title, file }, callback) {
+  const DBDocuments = []
 
+  try {
     const split = file.split(',')[1] // get rid of base64URL scheme header
     const parsedXLSX = XLSX.read(split, { type: 'base64' })
     const base = parsedXLSX.Sheets['база']
+    if (!base) {
+      throw new Error('В файле отсутствует страница с названием "база"')
+    }
+    const baseJSON = XLSX.utils.sheet_to_json(base)
 
-    const DBDocuments = []
-    for (const entry of base) {
+    for (const entry of baseJSON) {
       const DBDocument = {
         city: entry['Город'],
         address: entry['Адрес'],
       }
       DBDocuments.push(DBDocument)
     }
-    // db.collection('address-program-0')
   } catch (err) {
-    callback({ message: `Не удалось распарсить файл "${name}", по причине: ${err.message}` })
+    return callback({ message: `Не удалось распарсить файл "${title}", по причине: ${err.message}` })
   }
 
-  callback(null, 'got your file')
+  try {
+    const db = await require('../getMongoDB')()
+    const res = await db.collection('address-programs').insertOne({ title, rows: DBDocuments.length })
+    const newAddressProgramId = res.insertedId.toString()
+    const res2 = await db.collection(newAddressProgramId).insertMany(DBDocuments)
+  } catch (err) {
+    return callback({ message: `Не удалось сохранить файл "${title}" в базу данных, по причине: ${err.message}` })
+  }
+
+  callback(null)
 }
